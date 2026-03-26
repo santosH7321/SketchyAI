fc8ff6af9ca5294b8d8585ef8acaa1fa1b599270
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-26 18:46:40 +0530

    add History

--- components/right-sidebar.tsx
+++ components/right-sidebar.tsx
@@ -10,48 +10,56 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
+import { useEditorStore } from "@/store/useEditorState";
 
 export const RightSidebar = () => {
-  const isActive = true;
+  const { history, historyIndex ,setHistoryIndex} = useEditorStore();
   return (
     <aside className="flex h-full w-40 flex-col shrink-0 border-l border-zinc-800 bg-zinc-950/50 z-20 overflow-hidden">
       <div className="flex-1 min-h-0 w-full">
         <ScrollArea className="h-full w-full">
           <div className="flex flex-col gap-4 p-4 pb-4">
-            <div className="relative group">
-              <button
-                onClick={() => {}}
-                className={cn(
-                  "relative w-full aspect-square rounded-xl overflow-hidden border-2 transition-all duration-200",
-                  isActive
-                    ? "border-yellow-500 shadow-[0_0_15px_rgba(234,179,8,0.2)]"
-                    : "border-zinc-800 hover:border-zinc-600 opacity-60 hover:opacity-100",
-                )}
-              >
-                <Image
-                  width={500}
-                  height={500}
-                  src={"/logo.svg"}
-                  alt={`Version ${1}`}
-                  className="w-full h-full object-cover"
-                />
+            {
+              history.map((imgState, idx) => {
+                const isActive = historyIndex === idx;
+                return (
+                  <div className="relative group" key={idx}>
+                    <button
+                      onClick={() => {setHistoryIndex(idx)}}
+                      className={cn(
+                        "relative w-full aspect-square rounded-xl overflow-hidden border-2 transition-all duration-200",
+                        isActive
+                          ? "border-yellow-500 shadow-[0_0_15px_rgba(234,179,8,0.2)]"
+                          : "border-zinc-800 hover:border-zinc-600 opacity-60 hover:opacity-100",
+                      )}
+                    >
+                      <Image
+                        width={500}
+                        height={500}
+                        src={imgState}
+                        alt={`Version ${1}`}
+                        className="w-full h-full object-cover"
+                      />
 
-                {isActive && (
-                  <div className="absolute inset-0 bg-yellow-500/5 pointer-events-none" />
-                )}
-              </button>
+                      {isActive && (
+                        <div className="absolute inset-0 bg-yellow-500/5 pointer-events-none" />
+                      )}
+                    </button>
 
-              <div
-                className={cn(
-                  "absolute top-2 right-2 w-5 h-5 flex items-center justify-center rounded-full text-[10px] font-bold shadow-md z-10 pointer-events-none",
-                  isActive
-                    ? "bg-yellow-500 text-zinc-950"
-                    : "bg-zinc-800 text-zinc-400 border border-zinc-700",
-                )}
-              >
-                1
-              </div>
-            </div>
+                    <div
+                      className={cn(
+                        "absolute top-2 right-2 w-5 h-5 flex items-center justify-center rounded-full text-[10px] font-bold shadow-md z-10 pointer-events-none",
+                        isActive
+                          ? "bg-yellow-500 text-zinc-950"
+                          : "bg-zinc-800 text-zinc-400 border border-zinc-700",
+                      )}
+                    >
+                      {idx + 1}
+                    </div>
+                  </div>
+                )
+              })
+            }
           </div>
         </ScrollArea>
       </div>
--- store/useEditorState.ts
+++ store/useEditorState.ts
@@ -4,6 +4,10 @@ import { devtools } from "zustand/middleware"
 type EditorState = {
     image: string | null;
     prompt: string;
+    history: string[];
+    historyIndex: number;
+    setHistory: (history: string[]) => void;
+    setHistoryIndex: (index: number) => void;
     setImage: (imageData: string) => void;
     setPrompt: (prompt: string) => void;
     generateEdit: () => Promise<void>;
@@ -13,11 +17,21 @@ export const useEditorStore = create<EditorState>()(
     devtools((set, get) => ({
         image: null,
         prompt: "",
-        setImage: (imageData: string) => set({ image: imageData }),
+        history: [],
+        historyIndex: 0,
+        setImage: (imageData: string) => set({ image: imageData, history: [imageData] }),
+        setHistory: (history) => set({history}),
+        setHistoryIndex: (index: number) => {
+            const state = get();
+            return set({
+                historyIndex: index,
+                image: state.history[index]
+            });
+        },
+            
         generateEdit: async () => {
             const state = get();
-            console.log("Sending image and prompt to the server...");
-
+            
             const response = await fetch("/api/edit-image", {
                 method: 'POST',
                 headers: {
@@ -31,7 +45,14 @@ export const useEditorStore = create<EditorState>()(
             }
 
             const data = await response.json();
-            set(() => ({ image: data.result }))
+
+            const cloneHistory = [...state.history, data.result]
+
+            set(() => ({ 
+                image: data.result,
+                history: cloneHistory,
+                historyIndex: state.history.length,
+            }))
         },
         setPrompt: (prompt: string) => set({ prompt }),
     })),
