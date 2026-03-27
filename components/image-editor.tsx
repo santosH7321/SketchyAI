71260758cc05bad70c4f09e6d6b2d85571a82cda
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-27 12:01:41 +0530

    Draw the image on canvas

--- components/image-editor.tsx
+++ components/image-editor.tsx
@@ -1,23 +1,49 @@
 import { useEditorStore } from '@/store/useEditorState'
-import Image from 'next/image'
-import { useRef } from 'react'
+import { useCallback, useEffect, useRef } from 'react'
 
 const ImageEditor = () => {
     const canvasRef = useRef<HTMLCanvasElement>(null)
     const {image} = useEditorStore()
 
+    const draw = useCallback(() => {
+        if(!canvasRef.current) return;
+
+        const ctx = canvasRef.current.getContext("2d")
+
+        if(!ctx) return;
+
+        if(!image) return;
+        const img = new Image();
+        img.src = image;
+
+        img.onload = () => {
+            canvasRef.current!.width = img.naturalWidth;
+            canvasRef.current!.height = img.naturalHeight;
+            // clear the canvas
+            ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
+
+            // draw the image
+            ctx.drawImage(img, 0, 0, )
+        };
+    }, [image])
+
+    useEffect(() => {
+        if(!image) return;
+
+        const img = new Image();
+        img.src = image;
+
+        img.onload = () => {
+            draw()
+        };
+    }, [image, draw])
+
     return (
         <div className='w-full h-full flex items-center justify-center'>
             <canvas 
             ref={canvasRef}
-            className='border border-red-400 max-w-full max-h-full'
+            className='max-w-full max-h-full object-cover'
             ></canvas>
-            {/* <Image 
-                src={image as string} 
-                height="500" 
-                width="500" 
-                alt="img" 
-            /> */}
         </div>
     )
 }
