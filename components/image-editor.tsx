5fee085c26a3260a02742451304b4ad277e00ed3
Santosh Kumar <santoshkumar23kky@gmail.com>
2026-03-28 18:36:00 +0530

    add Upadating the mask

--- components/image-editor.tsx
+++ components/image-editor.tsx
@@ -1,10 +1,14 @@
 import { ToolType } from '@/lib/constants'
 import { useEditorStore } from '@/store/useEditorState'
+import { Point } from '@/types'
 import { useCallback, useEffect, useRef } from 'react'
 
 const ImageEditor = () => {
-    const canvasRef = useRef<HTMLCanvasElement>(null)
-    const imgRef = useRef<HTMLImageElement>(null)
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const maskCanvasRef = useRef<HTMLCanvasElement>(null);
+    const imgRef = useRef<HTMLImageElement>(null);
+    const startPosRef = useRef<Point | null>(null);
+
     const {image, selectedTool} = useEditorStore()
 
     const draw = useCallback(() => {
@@ -16,7 +20,6 @@ const ImageEditor = () => {
 
         ctx.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height);
         ctx.drawImage(imgRef.current, 0, 0, )
-
         
     }, [])
 
@@ -31,14 +34,72 @@ const ImageEditor = () => {
 
             canvasRef.current!.width = img.naturalWidth;
             canvasRef.current!.height = img.naturalHeight;
+
+            // prepare intial mask
+            maskCanvasRef.current = document.createElement("canvas");
+            maskCanvasRef.current.width = img.width;
+            maskCanvasRef.current.height = img.height;
+            
+            const maskCtx = maskCanvasRef.current.getContext('2d');
+
+            if(maskCtx) {
+                maskCtx.fillStyle = 'black';
+                maskCtx.fillRect(0, 0, maskCanvasRef.current.width, maskCanvasRef.current.height);
+
+            }
+
             draw()
         };
     }, [image, draw]);
 
     const startDrawing = (e: React.PointerEvent) => {
         if(selectedTool === ToolType.MOVE) return;
-        
+        if(e.pointerType !== 'mouse') return;
+
+        e.preventDefault();
+
+        const pos = getPointerPos(e);
+        startPosRef.current = pos;
+
+        if(selectedTool === ToolType.BRUSH || selectedTool === ToolType.ERASER) {
+            updateMask(pos, pos);
+        }
     };
+
+    const updateMask = (start: Point, end: Point) => {
+        if(!maskCanvasRef.current) return;
+
+        const ctx = maskCanvasRef.current.getContext('2d');
+        if(!ctx) return;
+
+        ctx.lineWidth = 100;
+        ctx.lineCap = "round"
+        ctx.lineJoin = "round"
+
+        if(selectedTool === ToolType.ERASER) {
+            ctx.strokeStyle = 'black'
+            ctx.fillStyle = 'black'
+        } else if(selectedTool === ToolType.BRUSH) {
+            ctx.strokeStyle = 'white'
+            ctx.fillStyle = 'white'
+        }
+
+
+        ctx.beginPath();
+        ctx.moveTo(start.x, start.y);
+        ctx.lineTo(end.x, end.y);
+        ctx.stroke();
+    };
+
+    const getPointerPos = ( e: React.PointerEvent) => {
+        if(!canvasRef.current) return {x: 0, y: 0};
+
+        const rect = canvasRef.current.getBoundingClientRect();
+        const x = (e.clientX - rect.left) * (canvasRef.current.width / rect.width);
+        const y = (e.clientY - rect.top) * (canvasRef.current.height / rect.height);
+
+        return {x, y};
+    }
 
     return (
         <div className='w-full h-full flex items-center justify-center'>
